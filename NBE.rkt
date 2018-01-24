06bea3cc5ed1e35080079cf50be8e2b3d7444186
Michael Arntzenius <daekharel@gmail.com>
2018-01-24 22:50:47 +0000

    update NBE.rkt

--- NBE.rkt
+++ NBE.rkt
@@ -1,9 +1,11 @@
 #lang racket
 
-;; Intensional normalisation by evaluation for the simply-typed lambda calculus.
+;; Intensional normalisation by evaluation for the untyped λ-calculus.
 ;; Based on http://homepages.inf.ed.ac.uk/slindley/nbe/nbe-cambridge2016.pdf
+;;
+;; Just for kicks, the s-expressions we use to represent λ-calculus terms are a
+;; subset of valid Racket expressions. This means you can (eval) them.
 
-(provide define-flat-contract)
 (define-syntax-rule (define-flat-contract name branches ...)
   (define name (opt/c (flat-rec-contract name branches ...))))
 
@@ -12,25 +14,28 @@
 (define-flat-contract term?
   name?
   (list/c term? term?)
-  (list/c 'lambda name? term?))
+  (list/c 'lambda (list/c name?) term?))
 
 ;; Normal here means β-short, but not necessarily η-long.
 (define-values (nf? ne?)
   (flat-murec-contract
-   ([nf? ne? (list/c 'lambda name? nf?)]
+   ([nf? ne? (list/c 'lambda (list/c name?) nf?)]
     [ne? name? (list/c ne? nf?)])
    (values nf? ne?)))
 
 ;; Semantic terms: either neutral terms or functions on semantic terms.
 (define sem/c (recursive-contract (or/c ne? (-> sem/c sem/c)) #:chaperone))
-(define env? (hash/c name? sem/c #:immutable #t))
+(define env/c (hash/c name? sem/c #:immutable #t))
 
-(define/contract (den term env)
-  (-> term? env? sem/c)
+(define/contract (den term [env (hash)])
+  (->* (term?) (env/c) sem/c)
   (match term
     ;; if x is bound, return its denotation; otherwise, return it as syntax.
     [(? name? x) (hash-ref env x x)]
-    [`(lambda ,x ,M) (lambda (v) (den M (hash-set env x v)))]
+    [`(lambda (,x) ,M)
+     ;; procedure-rename is a terrible hack to attach the parameter's name to
+     ;; the function so we can recover it later in reify.
+     (procedure-rename (lambda (v) (den M (hash-set env x v))) x)]
     [`(,M ,N) (app (den M env) (den N env))]))
 
 (define/contract (app f a)
@@ -40,11 +45,13 @@
 (define/contract (reify v)
   (-> sem/c nf?)
   (if (not (procedure? v)) v
-      (let ((x (gensym)))
-        `(lambda ,x ,(reify (v (reflect x)))))))
+      ;; terrible hack: use object-name to get the original parameter's name.
+      (let ((x (gensym (object-name v))))
+        `(lambda (,x) ,(reify (v (reflect x)))))))
 
 ;; we're intensional, so this is easy.
 (define/contract (reflect N) (-> ne? sem/c) N)
 
-(define (normalize M)
+(define/contract (norm M)
+  (-> term? nf?)
   (reify (den M (hash))))
